de8e09c22b99fce74b627805c476f3c41d771660
root <bartekwozniak93@gmail.com>
2016-12-13 14:41:18 +0000

    connect LCM1602 LCD and display text

---
+++ lcd.js
@@ -0,0 +1,10 @@
+require('mraa');
+var LCD = require('jsupm_i2clcd');
+
+var myLcd = new LCD.Lcm1602(0);
+
+myLcd.setCursor(0,0);
+myLcd.write('Hello World'); 
+
+myLcd.setCursor(0,1);
+myLcd.write('Bartek');
