371cbde4fb27bb2f1f3b77d08b399efbb98ab360
root <bartekwozniak93@gmail.com>
2016-12-17 15:47:48 +0000

    config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-	'album_name' : '', 
-	'album_key' : '' 
+	'album_name' : 'WorkingTimeRegistrationSystem', 
+	'album_key' : '0fb2ff2edfa5055fa37938a46ca18961bc2e0431bbd35cb928082f0b1126d4fa',
+
 }
