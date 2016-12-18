475823d06b936949acf5a2b11c7d8efde8557c35
root <bartekwozniak93@gmail.com>
2016-12-18 20:31:21 +0000

    improve camera funtions

--- camera/camera.js
+++ camera/camera.js
@@ -1,11 +1,5 @@
 var ffmpeg = require('fluent-ffmpeg');
-ffmpeg.setFfmpegPath('./ffmpeg-3.2-32bit-static/ffmpeg');
-
-function run_cmd(cmd, args, callBack ) {
-	var spawn = require('child_process').spawn;
-	var child = spawn(cmd, args);
-	var resp = "";
-}
+ffmpeg.setFfmpegPath('/project/camera/ffmpeg-3.2-32bit-static/ffmpeg');
 
 function getDateForName() {
 	var date = new Date();
@@ -26,30 +20,26 @@ function getFilePath(){
 }
 
 exports.delete = function (fileName) {
-	run_cmd("rm", ['-f', getFilePath()+fileName], function(text) {
-		console.log(text);
-	});
+	//	run_cmd("rm", ['-f', getFilePath()+fileName], function(text) {
+	//	console.log(text);
+	//});
+}
+
+exports.stream = function (req, res, callback){
+	
 }
  
 exports.take= function (req, res, callback) {
 	var fileName = getFileName();
-        var bartek = require('fluent-ffmpeg');	
-	bartek.setFfmpegPath('/project/camera/ffmpeg-3.2-32bit-static/ffmpeg');
-	var shot= bartek('/dev/video0')
+	var shot= ffmpeg('/dev/video0')
 		.inputFormat('v4l2')
 		.frames(1)
 		.save('/project/camera/temp_files/'+fileName)
 		.on('error', function(err, stdout, stderr) {
 			callback(req, res, '');
-			//console.log('Cannot process video: ' + err.message);
 		})
 		.on('end', function() {
 			callback(req, res, '/project/camera/temp_files/'+fileName);
-			//return '/camera/temp_files/'+fileName;
 		});
-	/*
-	run_cmd( "/project/camera/ffmpeg-3.2-32bit-static/ffmpeg", ['-s', '1280x720', '-f', 'video4linux2', '-i', '/dev/video0', '-vframes', '2', getFilePath()+getFileName()], function(text) { console.log (text) });
-	*/
-	//return '/camera/temp_files/'+fileName;
 }
 
